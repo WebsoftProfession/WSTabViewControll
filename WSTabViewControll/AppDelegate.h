ec6eccc4ffb4bbbb93641b03c501fd06afc8a775
WebsoftProfession <dotsquares@deepaksharma2.local>
2017-02-14 11:34:18 +0530

    Initial Commit

---
+++ WSTabViewControll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSTabViewControll
+//
+//  Created by Dotsquares on 2/14/17.
+//  Copyright © 2017 WebSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSTabViewControll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSTabViewControll
+//
+//  Created by Dotsquares on 2/14/17.
+//  Copyright © 2017 WebSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
